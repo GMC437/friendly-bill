b7dc85a37762109fbe08da42b70f4703c026ec76
Grant <grantmcnally.dev@gmail.com>
2020-04-27 01:57:01 +0100

    added action enums

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -8,3 +8,11 @@ export const StyleConstants = {
 export const APIConstants = {
   base: 'http://localhost:3002/',
 };
+
+export const ActionConstants = {
+    IS_BILL: 'IS_BILL',
+    FETCH_USER_DATA: 'FETCH_USER_DATA',
+    FETCH_USER_DATA_SAGA: 'FETCH_USER_DATA_SAGA',
+    FETCH_USER_DATA_SUCCESS: 'FETCH_USER_DATA_SUCCESS',
+    FETCH_USER_DATA_FAILURE: 'FETCH_USER_DATA_FAILURE',
+};
