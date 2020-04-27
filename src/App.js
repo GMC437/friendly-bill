4ea40802b46e7107c996801d51952988a267d51d
Grant <grantmcnally.dev@gmail.com>
2020-04-27 01:34:11 +0100

    wrapped app in theme provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { StyleConstants } from "./shared/constants";
 import welcomeIcon from "./assets/welcome.jpg";
+
+const { blue, yellow } = StyleConstants;
+const theme = {
+  blue,
+  yellow,
+};
 
 const App = () => {
   return (
     <Router>
-      <img src={welcomeIcon} alt="Welcome!" />
+      <ThemeProvider theme={theme}>
+        <img src={welcomeIcon} alt="Welcome!" />
+      </ThemeProvider>
     </Router>
   );
 };
