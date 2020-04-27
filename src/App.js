4def24fcd8b35bec395ffae1b050f59b69bfa799
Grant <grantmcnally.dev@gmail.com>
2020-04-27 01:45:36 +0100

    updated app to have state provider with store

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { StyleConstants } from "./shared/constants";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import appReducer from "./reducers";
+import rootSaga from "./sagas";
 import welcomeIcon from "./assets/welcome.jpg";
 
+const sagaMiddleware = createSagaMiddleware();
+const enhancers = applyMiddleware(sagaMiddleware);
+const store = createStore(appReducer, enhancers);
+sagaMiddleware.run(rootSaga);
 const { blue, yellow } = StyleConstants;
 const theme = {
   blue,
@@ -13,9 +22,11 @@ const theme = {
 const App = () => {
   return (
     <Router>
-      <ThemeProvider theme={theme}>
-        <img src={welcomeIcon} alt="Welcome!" />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <img src={welcomeIcon} alt="Welcome!" />
+        </ThemeProvider>
+      </Provider>
     </Router>
   );
 };
